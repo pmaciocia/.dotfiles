c94932ae2db67bf637d85d5b0bafc7468f4516d1
Peter Maciocia <peter.maciocia@gmail.com>
2012-10-11 16:45:51 -0700

    Only backup regular files

--- makeln.sh
+++ makeln.sh
@@ -7,35 +7,34 @@ ACTIVE=$BASE/active
 
 cd ~
 
-if [ ! -d "$BACKUP" ]
-then
-	mkdir $BACKUP
-fi
-
-if [ ! -d "$CURRENT" ]
-then
-	mkdir $CURRENT
-fi
+mkdir -p $CURRENT
+mkdir -p $ACTIVE
 
 for FILE in $ACTIVE/*; 
 do
 	NAME=$(basename $FILE)
-	DOTNAME=.$(basename $FILE);
+	DOTNAME=.$NAME;
 	if [ -e $DOTNAME ] 
 	then
-		echo "backing up $DOTNAME";
-		#echo "cp -r $DOTNAME $BACKUP";
-		cp -r $DOTNAME $BACKUP/;
+		if [ -L $DOTNAME ] 
+		then
+			# Remove any existing symbolic link
+			#echo "rm ~/.DOTNAME";
+			rm $DOTNAME
+		else
+			echo "backing up $DOTNAME";
+			# A regular file, so make a backup
+			#echo "cp -r $DOTNAME $BACKUP";
+			rsync -r $DOTNAME $BACKUP/;
+		fi
 	fi
 
-	#echo "cp -r $FILE $CURRENT"; 
+	# Copy from the active dir to current dir.
+	# This prevents conflicts from breaking the config
+	#echo "cp -r $FILE $CURRENT";
 	cp -r $FILE $CURRENT
-	if [ -L ~/$DOTNAME ] 
-	then
-		#echo "rm ~/.DOTNAME";
-		rm ~/$DOTNAME
-	fi
 
+	# Make the link from home -> current
  	#echo "ln -fvs $CURRENT/$NAME ~/$DOTNAME";
  	ln -fs $CURRENT/$NAME ~/$DOTNAME; 
 done
